0526f62021e6cc362cc1f037c075f9ebaa241c96
PerfectDay20 <zhang.stef@gmail.com>
2016-11-26 20:15:17 +0800

    add some easy python solutions

---
+++ LeetCode/242. Valid Anagram/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t): return False
+        d = {}
+        for i in s:
+            d[i] = d.get(i, 0) + 1
+        for i in t:
+            if i not in d or d[i] == 0:
+                return False
+            d[i] -= 1
+        return True
